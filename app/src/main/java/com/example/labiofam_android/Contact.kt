89f4188b00d262309d4b968c0472f1ca47e49a46
ManuelAlejandro <manuelalejandrogamboa35@gmail.com>
2023-10-19 23:24:55 +0200

    contactos actualizados

--- app/src/main/java/com/example/labiofam_android/Contact.kt
+++ app/src/main/java/com/example/labiofam_android/Contact.kt
@@ -3,6 +3,6 @@ package com.example.labiofam_android
 import android.util.Xml
 
 data class Contact(val name: String, val age:String, val position:String,
-                   val category: ContactsCategories, var phoneNumber: Int) {
+                   var phoneNumber: Int, var photo:String) {
 
 }

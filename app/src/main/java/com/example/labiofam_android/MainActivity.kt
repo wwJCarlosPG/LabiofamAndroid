c3dcc286c152942bc6b1c94eb63e099b16aae1bb
ManuelAlejandro <manuelalejandrogamboa35@gmail.com>
2023-10-20 16:44:57 +0200

    boton de bioproductos

--- app/src/main/java/com/example/labiofam_android/MainActivity.kt
+++ app/src/main/java/com/example/labiofam_android/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import com.example.labiofam_android.databinding.ActivityMainBinding
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment

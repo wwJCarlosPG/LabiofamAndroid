40ef1904306e246a242e62da513348fea962a92a
GamboaHMA <106393045+GamboaHMA@users.noreply.github.com>
2023-11-02 21:37:48 +0100

    comentar el costo de biop q no va en la cardView

--- app/src/main/java/com/example/labiofam_android/Bioproducts/BioproductsViewHolder.kt
+++ app/src/main/java/com/example/labiofam_android/Bioproducts/BioproductsViewHolder.kt
@@ -11,13 +11,13 @@ import com.example.labiofam_android.R
 class BioproductsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val bioproduct_name_tv:TextView = view.findViewById(R.id.bioproduct_name_tv)
-    private  val bioproduct_cost_tv:TextView = view.findViewById(R.id.bioproduct_cost_tv)
+    //private  val bioproduct_cost_tv:TextView = view.findViewById(R.id.bioproduct_cost_tv)
     private val  bioproduct_iv:ImageView = view.findViewById(R.id.bioproduct_iv)
     private val bioproduct_cv: CardView = view.findViewById(R.id.bioproduct_cv)
 
     fun render(bioproduct: Bioproducts, onItemSelected: (Bioproducts) -> Unit){
         bioproduct_name_tv.text = bioproduct.name
-        bioproduct_cost_tv.text = bioproduct.cost
+        //bioproduct_cost_tv.text = bioproduct.cost
 
         Glide.with(bioproduct_iv.context).load(bioproduct.photo).into(bioproduct_iv)
 
